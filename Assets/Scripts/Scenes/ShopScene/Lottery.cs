24800fc8e85e8474c69a018cb4ffc42c8430fadf
Ambai <21csc0001at@edu.sca.ac.jp>
2023-07-02 22:31:11 +0900

    laptopに移動

--- Assets/Scripts/Scenes/ShopScene/Lottery.cs
+++ Assets/Scripts/Scenes/ShopScene/Lottery.cs
@@ -6,73 +6,133 @@ public class Lottery : MonoBehaviour
 {
 
     PlayerDataManager playerDataManager;
-    public CardViewManager cardViewManager;// カードの見た目の処理
-    public CardDataManager cardData;// カードのデータを処理
+    CardDataManager cardData;       // カードのデータを処理
+
     [SerializeField]
-    //CardDataManager []cardDataManager;
+    List<int> CardRarity1List;
+    [SerializeField]
+    List<int> CardRarity2List;
+    [SerializeField]
+    List<int> CardRarity3List;
 
-    ShopController shopController;
+    List<int> ShopCards;
+    List<int> DeckAndShopCards; //現在の所持カード＋ショップに追加されたカードを格納する
 
-    List<CardDataManager> CardRarity1;
-    List<CardDataManager> CardRarity2;
-    List<CardDataManager> CardRarity3;
 
+
+
+    int MaxNumCards = 20;
+    
     void Start()
     {
-        for(int i = 1; i <= 20; i++)
+        for (int i = 1; i <= MaxNumCards; i++)
         {
             cardData = new CardDataManager(i);
             //各カードのレアリティに分ける
             if (cardData._cardRarity == 1)
             {
-                CardRarity1.Add(cardData._cardID);
-                Debug.Log(CardRarity1[i]);
+                CardRarity1List.Add(cardData._cardID);
             }
             if (cardData._cardRarity == 2)
             {
-                CardRarity2.Add(cardData);
+                CardRarity2List.Add(cardData._cardID);
             }
             if (cardData._cardRarity == 3)
             {
-                CardRarity3.Add(cardData);
+                CardRarity3List.Add(cardData._cardID);
             }
         }
-        
+
+        CardLottery(1);
     }
 
-
-    void Update()
+    /// <summary>
+    /// カードの抽選メソッド
+    /// </summary>
+    /// <param name="rarity">抽選したいレアリティ</param>
+    /// <returns>指定したレアリティのカードID (int)</returns>
+    int CardLottery(int rarity)
     {
-        
-    }
+        List<int> SelectedRarityList = null;
 
-    void zako()     //レア度１が２枚、２が１枚 
-    {
-        
-    }
-
-    void kyoutakara()   //レア度1が１枚、２が２枚
-    {
-
-    }
-
-    void boss()     //レア度２が２枚、３が１枚
-    {
-
-    }
-
-    void shop()     //レア度2が１枚、レア度１が２枚、回復が１枚（確定）
-    {
-        int shopLottery2 = Random.Range(0, CardRarity2.Count);
-        int rare2 = 0;
-        for(int i = 0; i < playerDataManager._deckList.Count; i++)  //持っているカードの枚数分回す
+        //引数で指定されたレアリティのListをSelectedRarityListへ代入
+        switch (rarity)
         {
-            if (CardRarity2[shopLottery2]._cardID == playerDataManager._deckList[i])　　//抽選されたIDが持っているカードにあるか？
-            {
-                continue;
-            }
-            rare2 = CardRarity2[shopLottery2]._cardID;
+            case 1:
+                SelectedRarityList = CardRarity1List;
+                break;
+            case 2:
+                SelectedRarityList = CardRarity2List;
+                break;
+            case 3:
+                SelectedRarityList = CardRarity3List;
+                break;
+            default:
+                Debug.Log("指定されたレアリティがありません。");
+                return -1;
         }
-        shopController.ShowCards(rare2, 1, 1);
+
+        //所持カードとショップに出ているカードをDeckAndShopCardsへ追加
+        DeckAndShopCards.AddRange(playerDataManager._deckList);
+        DeckAndShopCards.AddRange(ShopCards);
+
+        int cardLottery;
+
+        //抽選処理
+        while (true)
+        {
+            cardLottery = Random.Range(0, SelectedRarityList.Count);   // 指定されたレアリティのListのからランダムに要素を選択
+            bool hasCards = false;
+
+            for (int i = 0; i < DeckAndShopCards.Count; i++)  // 所持カードとショップに出ているカードの枚数分回す
+            {
+                if (SelectedRarityList[cardLottery] == DeckAndShopCards[i])  // 抽選されたカードを所持していたら再抽選
+                {
+                    hasCards = true;
+                    break;
+                }
+            }
+            
+            if (!hasCards) // 抽選されたカードを所持していなかったら抽選終了
+                break;
+        }
+
+        DeckAndShopCards = null;
+        return cardLottery;
     }
+
+
+    public (int,int,int) ShopLottery()     // レア度2が１枚、レア度1が２枚、回復が１枚（確定）
+    {
+        
+        int cardLottery1 = CardLottery(2);
+        ShopCards.Add(cardLottery1);     // ショップに追加するカードはListに格納する
+
+        int cardLottery2 = CardLottery(1);
+        ShopCards.Add(cardLottery2);
+
+        int cardLottery3 = CardLottery(1);
+        ShopCards.Add(cardLottery3);
+        
+        return (cardLottery1, cardLottery2, cardLottery3);
+
+
+    }
+
+    void zako()     // レア度1が２枚、レア度2が１枚 
+    {
+        
+    }
+
+    void kyoutakara()   // レア度1が１枚、レア度2が２枚
+    {
+
+    }
+
+    void boss()     // レア度2が２枚、レア度3が１枚
+    {
+
+    }
+
+
 }
--- Assets/Scripts/Scenes/ShopScene/ShopController.cs
+++ Assets/Scripts/Scenes/ShopScene/ShopController.cs
@@ -7,32 +7,44 @@ public class ShopController : MonoBehaviour
     PlayerDataManager playerDataManager;
     CardController cardController;
 
+    Lottery lottery;
+
     bool isHeelPotion = false;
 
     private void Start()
     {
-        playerDataManager = GameManager.Instance.playerData;
-        foreach (var deck in playerDataManager._deckList)
-        {
-            if (deck == 3)
-            {
-                isHeelPotion = true;
-            }
+        //playerDataManager = GameManager.Instance.playerData;
+        //foreach (var deck in playerDataManager._deckList)
+        //{
+        //    if (deck == 3)
+        //    {
+        //        isHeelPotion = true;
+        //    }
 
-        }
+        //}
+
+        ////プレイヤーのデッキを作成
+        //deckNumberList = playerData._deckList;
+        //for (int init = 0; init < deckNumberList.Count; init++)// デッキの枚数分
+        //{
+        //    CardController card = Instantiate(cardPrefab, CardPlace);//カードを生成する
+        //    card.name = "Deck" + init.ToString();//生成したカードに名前を付ける
+        //    card.Init(deckNumberList[init]);//デッキデータの表示
+        //}
 
     }
     void Update()
     {
 
 
-        playerDataManager._money -= 50;
-        playerDataManager._deckList.Add(1);
+        //playerDataManager._money -= 50;
+        //playerDataManager._deckList.Add(1);
 
     }
 
-    public void ShowCards(int rare2, int rare1_1, int rare1_2)
+    public void ShowCards()
     {
+        (int Card1, int Card2, int Card3) = lottery.ShopLottery();     // メモ: タプルと言って複数の戻り値を受け取れる
         cardController.Init(rare2); 
         
     }
