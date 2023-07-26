1c7a5b4ef65f08723efa317e3f81dd225001b20e
Toshiharu-Kuriyama <21csc0006kt@edu.sca.ac.jp>
2023-07-26 23:50:15 +0900

    焚火シーンのカードの強化を実装、それに伴い強化用カードの作成

--- Assets/Scripts/Scenes/BonfireScene/BonfireManager.cs
+++ Assets/Scripts/Scenes/BonfireScene/BonfireManager.cs
@@ -8,7 +8,8 @@ public class BonfireManager : MonoBehaviour
 
     //カード
     [SerializeField] CardController cardPrefab;
-    [SerializeField] Transform CardPlace;
+    [SerializeField] Transform upperCardPlace;
+    [SerializeField] Transform lowerCardPlace;
     List<int> deckNumberList;                    //プレイヤーのもつデッキナンバーのリスト
 
     Vector3 CardScale = Vector3.one * 0.25f;     // 生成するカードのスケール
@@ -16,20 +17,66 @@ public class BonfireManager : MonoBehaviour
 
     void Start()
     {
-        playerData = GameManager.Instance.playerData;
+        //playerData = GameManager.Instance.playerData;
+        playerData = new PlayerDataManager("Wizard");
         InitDeck();
     }
 
     private void InitDeck() //デッキ生成
     {
         deckNumberList = playerData._deckList;
-        for (int init = 0; init < deckNumberList.Count; init++)// デッキの枚数分
+        int distribute = DistributionOfCards(deckNumberList.Count);
+        if (distribute <= 0) //デッキの枚数が0枚なら生成しない
+            return;
+        for (int init = 1; init <= deckNumberList.Count; init++)// デッキの枚数分
         {
-            CardController card = Instantiate(cardPrefab, CardPlace);//カードを生成する
-            card.transform.localScale = CardScale;
-            card.name = "Deck" + init.ToString();//生成したカードに名前を付ける
-            card.Init(deckNumberList[init]);//デッキデータの表示
+            if (init <= distribute) //決められた数をupperCardPlaceに生成する
+            {
+                CardController card = Instantiate(cardPrefab, upperCardPlace);//カードを生成する
+                card.transform.localScale = CardScale;
+                card.name = "Deck" + (init-1).ToString();//生成したカードに名前を付ける
+                card.Init(deckNumberList[init - 1]);//デッキデータの表示
+            }
+            else //残りはlowerCardPlaceに生成する
+            {
+                CardController card = Instantiate(cardPrefab, lowerCardPlace);//カードを生成する
+                card.transform.localScale = CardScale;
+                card.name = "Deck" + (init-1).ToString();//生成したカードに名前を付ける
+                card.Init(deckNumberList[init - 1]);//デッキデータの表示
+            }
         }
+    }
+
+    /// <summary>
+    /// デッキのカード枚数によって上下のCardPlaceに振り分ける数を決める
+    /// </summary>
+    /// <param name="deckCount">デッキの枚数</param>
+    /// <returns>上のCardPlaceに生成するカードの枚数</returns>
+    int DistributionOfCards(int deckCount) 
+    {
+        int distribute = 0;
+        if (0 <= deckCount && deckCount <= 5)//デッキの数が0以上5枚以下だったら 
+        {
+            distribute = deckCount;//デッキの枚数分生成
+        }
+        else if (deckCount > 5)//デッキの数が6枚以上だったら
+        {
+            if (deckCount % 2 == 0)//デッキの枚数が偶数だったら
+            {
+                int value = deckCount / 2;
+                distribute = value;//デッキの半分の枚数を生成
+            }
+            else //デッキの枚数が奇数だったら
+            {
+                int value = (deckCount - 1) / 2;
+                distribute = value + 1;//デッキの半分+1の枚数を生成
+            }
+        }
+        else //デッキの数が0枚未満だったら
+        {
+            distribute = 0;//生成しない
+        }
+        return distribute;
     }
 
     /// <summary>
@@ -39,6 +86,17 @@ public class BonfireManager : MonoBehaviour
     public void CardEnhance(GameObject selectCard)
     {
         Debug.Log("カード強化！");
-
+        int id = selectCard.GetComponent<CardController>().cardDataManager._cardID; //選択されたカードのIDを取得
+        for (int count = 0; count < deckNumberList.Count; count++)
+        {
+            if (deckNumberList[count] == id) //デッキからIDのカードを探す 
+            {
+                if(deckNumberList[count] <= 20 && id != 3) //IDのカードが未強化で魔女の霊薬でなければ
+                {
+                    deckNumberList[count] += 100; //そのカードの強化版のIDに変更する
+                }
+            }
+            Debug.Log("現在のPlayerのデッキリストは：" + deckNumberList[count]);
+        }
     }
 }
